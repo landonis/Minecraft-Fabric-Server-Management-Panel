47061f028a556608de0153a0c8524fec64237e0a
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2025-07-08 17:44:56 -0400

    Update index.d.ts

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,5 +1,11 @@
-declare namespace Express {
-  export interface Request {
-    user?: import("../User").User;
+import { User } from '../User';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
   }
 }
+
+export {}; // to make it a module
